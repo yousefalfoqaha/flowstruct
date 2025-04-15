c94c495c42a750f2316ed2443e18f4cf79b53c2f
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-04-15 19:53:47 +0300

    added logs route

--- legacy-dashboard/legacy-dashboard/src/routes/_layout/study-plans/route.tsx
+++ legacy-dashboard/legacy-dashboard/src/routes/_layout/study-plans/route.tsx
@@ -1,8 +1,18 @@
 import {createFileRoute, Outlet} from '@tanstack/react-router'
+import {Stack, Title} from "@mantine/core";
 
 export const Route = createFileRoute('/_layout/study-plans')({
     loader: () => ({
         crumb: 'Study Plans',
     }),
-    component: () => <Outlet/>,
-})
+    component: RouteComponent,
+});
+
+function RouteComponent() {
+    return (
+        <Stack>
+            <Title order={2} fw={600}>Programs</Title>
+            <Outlet/>
+        </Stack>
+    );
+}
