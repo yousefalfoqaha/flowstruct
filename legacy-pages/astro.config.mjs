61e2679fd0c22f805d0cddacc2a5fb1ca5d9288c
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-05-01 21:43:41 +0300

    added cloudflare as a dependency

--- legacy-pages/astro.config.mjs
+++ legacy-pages/astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [tailwind()]
 });
