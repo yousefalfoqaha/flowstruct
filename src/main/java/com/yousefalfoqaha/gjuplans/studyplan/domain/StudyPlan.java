3629de71368bbb8348fc4480618c8ece137fce81
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-06-04 15:22:09 +0300

    optimize imports

--- src/main/java/com/yousefalfoqaha/gjuplans/studyplan/domain/StudyPlan.java
+++ src/main/java/com/yousefalfoqaha/gjuplans/studyplan/domain/StudyPlan.java
@@ -1,8 +1,6 @@
 package com.yousefalfoqaha.gjuplans.studyplan.domain;
 
 import com.yousefalfoqaha.gjuplans.program.domain.Program;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
