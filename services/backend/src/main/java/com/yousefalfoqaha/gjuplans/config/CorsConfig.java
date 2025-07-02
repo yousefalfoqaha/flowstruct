4eb0b24db2c246c944862742d7df087bb049a3b6
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-02 15:38:56 +0300

    profile-aware cors configuration

--- services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
+++ services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.yousefalfoqaha.gjuplans.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,9 +13,22 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    @Profile("dev")
+    public CorsConfigurationSource devCorsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:4321", "http://localhost:5173"));
+        return getCorsConfigurationSource(config);
+    }
+
+    @Bean
+    @Profile("prod")
+    public CorsConfigurationSource prodCorsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://admin.gjuplans.com"));
+        return getCorsConfigurationSource(config);
+    }
+
+    private CorsConfigurationSource getCorsConfigurationSource(CorsConfiguration config) {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -23,4 +37,6 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+
+
 }
