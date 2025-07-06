3d18855a66f0ed1949333fef3f1c9fab96960674
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-06 16:51:17 +0300

    async

--- apps/dashboard/src/routes/index.tsx
+++ apps/dashboard/src/routes/index.tsx
@@ -3,9 +3,9 @@ import { MeQuery } from '@/features/user/queries.ts';
 import { DefaultSearchValues } from '@/utils/defaultSearchValues.ts';
 
 export const Route = createFileRoute('/')({
-  beforeLoad: ({ context: { queryClient } }) => {
+  beforeLoad: async ({ context: { queryClient } }) => {
     try {
-      queryClient.ensureQueryData(MeQuery);
+      await queryClient.ensureQueryData(MeQuery);
     } catch {
       throw redirect({ to: '/login' });
     }
