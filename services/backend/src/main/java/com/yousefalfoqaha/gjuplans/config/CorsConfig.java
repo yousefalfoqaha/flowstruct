b64ac98b3b028007fc00e53d45157fa69027aee4
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-06 16:27:17 +0300

    allowed origins string parsed correctly

--- services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
+++ services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
@@ -3,23 +3,27 @@ package com.yousefalfoqaha.gjuplans.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Value("${cors.allowed-origins}")
-    private List<String> allowedOrigins;
+    private String allowedOrigins;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedOrigins(
+                Arrays.stream(allowedOrigins.split(","))
+                        .map(String::trim)
+                        .toList()
+        );
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
