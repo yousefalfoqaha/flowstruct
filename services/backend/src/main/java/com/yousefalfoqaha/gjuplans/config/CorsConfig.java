b1fdb1be84c7f28d77d75c1fd4982fce813fae53
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-09 14:01:51 +0300

    branch init

--- services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
+++ services/backend/src/main/java/com/yousefalfoqaha/gjuplans/config/CorsConfig.java
@@ -18,6 +18,9 @@ public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        System.out.println(Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .toList());
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
                 Arrays.stream(allowedOrigins.split(","))
