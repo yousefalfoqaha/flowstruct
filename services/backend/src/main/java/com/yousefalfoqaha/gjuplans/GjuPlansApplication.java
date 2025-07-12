91913a73d3ac877e3302c578918f3422e3b48d0b
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-12 19:32:21 +0300

    test

--- services/backend/src/main/java/com/yousefalfoqaha/gjuplans/GjuPlansApplication.java
+++ services/backend/src/main/java/com/yousefalfoqaha/gjuplans/GjuPlansApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+//
 @SpringBootApplication
 public class GjuPlansApplication {
 
