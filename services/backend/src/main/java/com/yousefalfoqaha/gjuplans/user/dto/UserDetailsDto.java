827af7fa4006a3156b90264e688556f6209bb34d
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-07-26 12:23:44 +0300

    edit user details api working

--- services/backend/src/main/java/com/yousefalfoqaha/gjuplans/user/dto/UserDetailsDto.java
+++ services/backend/src/main/java/com/yousefalfoqaha/gjuplans/user/dto/UserDetailsDto.java
@@ -1,14 +1,19 @@
 package com.yousefalfoqaha.gjuplans.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UserDetailsDto(
 
         @NotBlank(message = "Username required.")
+        @Size(min = 3, message = "Username must be at least 3 characters long.")
         String username,
 
+        @NotNull(message = "Undefined new password field.")
         String newPassword,
 
+        @NotNull(message = "Undefined confirm password field.")
         String confirmPassword
 ) {
 }
