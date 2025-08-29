30f0a68202fc23d0c7e79c64056322eaaa9463cb
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-08-29 20:18:08 +0300

    update study plan link generation to dynamically use protocol and domain environment variables

--- client/apps/cms/src/features/study-plan/components/StudyPlanLinkModalContent.tsx
+++ client/apps/cms/src/features/study-plan/components/StudyPlanLinkModalContent.tsx
@@ -1,23 +1,28 @@
 import { Anchor, Button, CopyButton, Group, Stack } from '@mantine/core';
 import { Copy, ExternalLink } from 'lucide-react';
+import { STUDY_PLAN_ENDPOINT } from '@/features/study-plan/constants.ts';
+
+const CONTENT_DOMAIN = import.meta.env.VITE_CONTENT_DOMAIN;
+const SECURE = import.meta.env.VITE_SECURE;
+const PROTOCOL = SECURE ? 'https' : 'http';
 
 type Props = {
   studyPlanId: number;
 };
 
 export function StudyPlanLinkModalContent({ studyPlanId }: Props) {
-  const studentUrl = `${import.meta.env.VITE_STUDENT_BASE_URL}/study-plans/${studyPlanId}`;
+  const link = `${PROTOCOL}://${CONTENT_DOMAIN}${STUDY_PLAN_ENDPOINT}/${studyPlanId}`;
 
   return (
     <Stack>
       <Group justify="space-between">
         <Group>
           <ExternalLink color="gray" size={14} />
-          <Anchor style={{ flex: 1 }} href={studentUrl} target="_blank" rel="noopener noreferrer">
-            {studentUrl}
+          <Anchor style={{ flex: 1 }} href={link} target="_blank" rel="noopener noreferrer">
+            {link}
           </Anchor>
         </Group>
-        <CopyButton value={studentUrl}>
+        <CopyButton value={link}>
           {({ copied, copy }) => (
             <Button
               leftSection={copied ? null : <Copy size={18} />}
