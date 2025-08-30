dd4f81d1ac2076f989a0476f7635b0166e4553b5
yousefalfoqaha <yousefmustafaadam@gmail.com>
2025-08-30 18:51:50 +0300

    refactor: update environment variable usage and formatting

--- client/apps/cms/src/features/study-plan/components/StudyPlanLinkModalContent.tsx
+++ client/apps/cms/src/features/study-plan/components/StudyPlanLinkModalContent.tsx
@@ -1,42 +1,42 @@
-import { Anchor, Button, CopyButton, Group, Stack } from '@mantine/core';
-import { Copy, ExternalLink } from 'lucide-react';
-import { STUDY_PLAN_ENDPOINT } from '@/features/study-plan/constants.ts';
+import {Anchor, Button, CopyButton, Group, Stack} from '@mantine/core';
+import {Copy, ExternalLink} from 'lucide-react';
+import {STUDY_PLAN_ENDPOINT} from '@/features/study-plan/constants.ts';
 
-const CONTENT_DOMAIN = import.meta.env.VITE_CONTENT_DOMAIN
-  ? import.meta.env.VITE_CONTENT_DOMAIN
-  : 'localhost:4321';
+const CONTENT_DOMAIN = import.meta.env.VITE_DOMAIN
+    ? import.meta.env.VITE_CONTENT_DOMAIN
+    : 'localhost:4321';
 
 const SECURE = import.meta.env.VITE_SECURE ? import.meta.env.VITE_SECURE : false;
 const PROTOCOL = SECURE ? 'https' : 'http';
 
 type Props = {
-  studyPlanId: number;
+    studyPlanId: number;
 };
 
-export function StudyPlanLinkModalContent({ studyPlanId }: Props) {
-  const link = `${PROTOCOL}://${CONTENT_DOMAIN}${STUDY_PLAN_ENDPOINT}/${studyPlanId}`;
+export function StudyPlanLinkModalContent({studyPlanId}: Props) {
+    const link = `${PROTOCOL}://${CONTENT_DOMAIN}${STUDY_PLAN_ENDPOINT}/${studyPlanId}`;
 
-  return (
-    <Stack>
-      <Group justify="space-between">
-        <Group>
-          <ExternalLink color="gray" size={14} />
-          <Anchor style={{ flex: 1 }} href={link} target="_blank" rel="noopener noreferrer">
-            {link}
-          </Anchor>
-        </Group>
-        <CopyButton value={link}>
-          {({ copied, copy }) => (
-            <Button
-              leftSection={copied ? null : <Copy size={18} />}
-              color={copied ? 'teal' : 'blue'}
-              onClick={copy}
-            >
-              {copied ? 'Copied' : 'Copy URL'}
-            </Button>
-          )}
-        </CopyButton>
-      </Group>
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Group justify="space-between">
+                <Group>
+                    <ExternalLink color="gray" size={14}/>
+                    <Anchor style={{flex: 1}} href={link} target="_blank" rel="noopener noreferrer">
+                        {link}
+                    </Anchor>
+                </Group>
+                <CopyButton value={link}>
+                    {({copied, copy}) => (
+                        <Button
+                            leftSection={copied ? null : <Copy size={18}/>}
+                            color={copied ? 'teal' : 'blue'}
+                            onClick={copy}
+                        >
+                            {copied ? 'Copied' : 'Copy URL'}
+                        </Button>
+                    )}
+                </CopyButton>
+            </Group>
+        </Stack>
+    );
 }
